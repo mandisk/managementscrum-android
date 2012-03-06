751bfc9f94432e369d82d7ab99dfefb1bce957bb
jorge.pb1987@gmail.com <jorge.pb1987@gmail.com@7b80908d-1b50-f8fa-40dc-71584b09bdcc>
2012-03-06 18:59:46 +0000

    Primer commit!!!!!!

--- MScrumAndroid/src/org/minftel/mscrum/activities/LoginActivity.java
+++ MScrumAndroid/src/org/minftel/mscrum/activities/LoginActivity.java
@@ -3,11 +3,11 @@ package org.minftel.mscrum.activities;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MScrumAndroidActivity extends Activity {
+public class LoginActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.login);
     }
 }
