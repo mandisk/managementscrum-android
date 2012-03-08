0cc390256b82239266de7065aa2b2cfc4b63db38
mandisk@gmail.com <mandisk@gmail.com@7b80908d-1b50-f8fa-40dc-71584b09bdcc>
2012-03-08 19:37:05 +0000

    Carga de proyectos

--- MScrumAndroid/src/org/minftel/mscrum/activities/ProjectActivity.java
+++ MScrumAndroid/src/org/minftel/mscrum/activities/ProjectActivity.java
@@ -3,67 +3,64 @@ package org.minftel.mscrum.activities;
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.minftel.mscrum.model.ProjectDetail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.minftel.mscrum.utils.JSONConverter;
 import org.minftel.mscrum.utils.TextAdapter;
 
 public class ProjectActivity extends ListActivity {
-    /** Called when the activity is first created. */
+	/** Called when the activity is first created. */
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.project);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.project);
 
-            Context ctx = getApplicationContext();
-            Resources res = ctx.getResources();
+		Context ctx = getApplicationContext();
 
-            String[] options = res.getStringArray(R.array.title_names);
+		int i = 0;
+		Intent intent = getIntent();
+		Bundle datos = intent.getExtras();
+		String json = datos.getString("projects");
+		List<ProjectDetail> projects = new ArrayList<ProjectDetail>();
+		
+		try {
+			projects = JSONConverter.fromJSONtoProjecList(json);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-            setListAdapter(new TextAdapter(ctx, R.layout.list_item,options));
+		// Initialize Array String
+		String[] options = new String[projects.size()];
 
-    }
+		for (ProjectDetail project : projects) {
+			options[i] = project.getName();
+			i++;
+		}
 
-    public void onListItemClick(ListView parent, View v, int position, long id) {
-            Intent intent = null;
-            // Show position clicked
-            
-              Toast.makeText(getApplicationContext(), "You have selected " +
-              (position + 1) + "th item", Toast.LENGTH_SHORT).show();
-             
-         /*   switch (position) {
-            case 0:
-                    // Option --> EMERGENCY
-                    intent = new Intent(MainActivity.this, EmergencyActivity.class);
-                    startActivity(intent);
-                    break;
-            case 1:
-                    // Option --> ASSISTANCE
-                    intent = new Intent(MainActivity.this, AssistanceActivity.class);
-                    startActivity(intent);
-                    break;
-            case 2:
-                    // Option --> WALK
-                    intent = new Intent(MainActivity.this, WalkMainActivity.class);
-                    startActivity(intent);
-                    break;
-            case 3:
-                    // Option --> CONSULTATION
-                    intent = new Intent(MainActivity.this, ConsultActivity.class);
-                    startActivity(intent);
-                    break;
-            case 4:
-                    // Option --> CONFIGURATION
-                    intent = new Intent(MainActivity.this, ConfigActivity.class);
-                    startActivity(intent);
-                    break;
-            }*/
+		// Load data in ListAdapter
+		setListAdapter(new TextAdapter(ctx, R.layout.list_item, options));
 
-    }
+	}
+
+	public void onListItemClick(ListView parent, View v, int position, long id) {
+		//Intent intent = null;
+		// Show position clicked
+
+		Toast.makeText(getApplicationContext(),
+				"You have selected " + (position + 1) + "th item",
+				Toast.LENGTH_SHORT).show();
+
+	}
 
 }
