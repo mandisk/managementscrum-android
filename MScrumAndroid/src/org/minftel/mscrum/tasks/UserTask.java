09fa94d57e0b94762fb3e69deea2ad0feb76472c
mandisk@gmail.com <mandisk@gmail.com@7b80908d-1b50-f8fa-40dc-71584b09bdcc>
2012-03-14 19:48:12 +0000

    Añadido UserTask

---
+++ MScrumAndroid/src/org/minftel/mscrum/tasks/UserTask.java
@@ -0,0 +1,118 @@
+package org.minftel.mscrum.tasks;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.minftel.mscrum.activities.ProjectActivity;
+import org.minftel.mscrum.activities.R;
+import org.minftel.mscrum.utils.ScrumConstants;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class UserTask extends AsyncTask<String, Integer, String>{
+
+	private ProjectActivity activity;
+	private ProgressDialog progressDialog;
+	
+	public UserTask(Activity activity) {
+		this.activity =  (ProjectActivity) activity;
+		this.progressDialog = new ProgressDialog(activity);
+	}	
+	
+	@Override
+	protected String doInBackground(String... params) {
+		
+		String result = null;
+		
+		try {
+			Log.i(ScrumConstants.TAG, "Showing users...");
+			
+			String sessionId = activity.getSharedPreferences(
+					ScrumConstants.SHARED_PREFERENCES_FILE, 
+					Activity.MODE_PRIVATE).getString(ScrumConstants.SESSION_ID, "");
+			String url = ScrumConstants.BASE_URL + ScrumConstants.SESSION_URL + sessionId;			
+			
+			URL urlDispatcher = new URL(url);
+			URLConnection connection = urlDispatcher.openConnection();
+						
+			connection.setDoInput(true);
+			connection.setDoOutput(true);
+			
+			// To send to the server
+			OutputStream out = connection.getOutputStream();
+			DataOutputStream dos = new DataOutputStream(out);
+						
+			dos.writeInt(ScrumConstants.ACTION_REQUEST_LIST_USERS);
+			dos.writeUTF(params[0]); // project ID
+			
+			// To receive from the server
+			InputStream in = connection.getInputStream();
+			DataInputStream dis = new DataInputStream(in);
+			
+			result = dis.readUTF();
+			
+			dos.close();
+			dis.close();
+			
+			connection = null;
+			
+		} catch(MalformedURLException e) {
+			Log.e(ScrumConstants.TAG, "MalformedURLException: " + e.getMessage());
+		} catch (IOException e) {
+			Log.e(ScrumConstants.TAG, "IOException: " + e.getMessage());
+		}
+		
+		return result;
+	}
+	
+	@Override
+	protected void onPostExecute(String result) {
+		if (progressDialog.isShowing()) {
+			progressDialog.dismiss();
+		}
+		
+		if (result != null) {
+			
+			if (result.equals(ScrumConstants.SESSION_EXPIRED)) {
+				Log.w(ScrumConstants.TAG, "Session expired");
+				return;
+			}
+			
+			
+			try {
+				JSONObject json = new JSONObject(result);
+				JSONArray jsonUsers = json.getJSONArray("users");
+				
+				Log.i(ScrumConstants.TAG, jsonUsers.toString());
+				
+				// Send broadcast to open ProjectActivity
+				Intent broadCastIntent = new Intent();
+				broadCastIntent.setAction(ScrumConstants.BROADCAST_GO_USERS);
+				broadCastIntent.putExtra("users", jsonUsers.toString());
+				this.activity.sendBroadcast(broadCastIntent);
+				
+			} catch (JSONException e) {
+				Log.e(ScrumConstants.TAG, "JSONException: " + e.getMessage());
+			}
+		}
+	}
+
+	@Override
+	protected void onPreExecute() {
+		progressDialog.setMessage(activity.getResources().getString(R.string.dialog_loading));
+		progressDialog.show();
+	}
+}
